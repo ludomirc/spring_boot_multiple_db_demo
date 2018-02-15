4748c2e50727cbe4922f12b88b0e522cf8555578
benek <beniamin.czaplicki@gmail.com>
2018-02-15 19:57:38 +0100

    update properties

--- src/main/java/org/qbit/datasource/demo/web/TestResource.java
+++ src/main/java/org/qbit/datasource/demo/web/TestResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @RestController
@@ -18,14 +19,15 @@ public class TestResource {
     TService t1;
 
     @GetMapping("/data")
-    public String getData(){
+    public List getData(){
 
+        LinkedList list = new LinkedList();
         List<Table1> table1List =  t1.getAllFromT1();
-        System.out.println("table 1 list: " + table1List);
-
         List<Table2> table2List =  t1.getAllFromT2();
-        System.out.println("table 2 list: " + table2List);
 
-        return "test";
+        list.add(table1List);
+        list.add(table2List);
+
+        return list;
     }
 }
